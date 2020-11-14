5295f3ef3224044a793b04063643d1b40973046b
zara <zarazolfaghari76@gmail.com>
2020-11-14 23:32:41 +0330

    added media root

--- btre_project/settings.py
+++ btre_project/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'btre_project/static')
 ]
+
+# Media Folder
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
