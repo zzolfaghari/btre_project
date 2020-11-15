6bbb739765e86874c333941a7c1544e5108b4793
zara <zarazolfaghari76@gmail.com>
2020-11-15 19:48:17 +0330

    refactored admin

--- listings/admin.py
+++ listings/admin.py
@@ -3,9 +3,9 @@ from .models import Listing
 
 
 class ListAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtors')
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
     list_display_links = ('id', 'title')
-    list_filter = ('realtors',)
+    list_filter = ('realtor',)
     list_editable = ('is_published',)
     search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
     list_per_page = 25
