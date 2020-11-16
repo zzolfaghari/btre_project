62ce3b7f74aa7ce5aeb3cc9c95b766bd0018a88a
zara <zarazolfaghari76@gmail.com>
2020-11-16 11:49:33 +0330

    added message to register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'Post':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
