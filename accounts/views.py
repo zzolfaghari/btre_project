111e8e8b5f794d899e29b65ba0112326f001dba0
zara <zarazolfaghari76@gmail.com>
2020-11-16 15:18:15 +0330

    refactored register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'Post':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is taken')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                                                    first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'you are now registered and can log in')
+                    return redirect('login')
     else:
+        messages.error(request, "password do not match")
         return render(request, 'accounts/register.html')
 
 
