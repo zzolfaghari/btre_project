e66b31d70582221b659d1aed1049c8cbfdfd6e24
zara <zarazolfaghari76@gmail.com>
2020-11-23 17:38:17 +0330

    added pagination

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Listing
@@ -5,11 +6,15 @@ from .models import Listing
 
 def index(request):
     listings = Listing.objects.all()
+    paginator = Paginator(Listing, 4)  # Show 4 listings per page.
+
+    page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
     context = {'listings': listings}
     return render(request, 'listing/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listing/listing.html')
 
 
